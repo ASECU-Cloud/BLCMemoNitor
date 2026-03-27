70c43e6d35192655f67a85aa35551f08d9aaaeb6
ASECU Pseudo <agunglioko@gmail.com>
2026-03-27 13:44:15 +0700

    adding default super admin for general purpose.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'BLC Super Admin',
+            'email' => 'blcmemonitor@diskominfo.gov',
+            'password' => bcrypt("BLCMemonitor2026!")
         ]);
     }
 }
